2772c3002b05d67d57b7aab6addf6b6884a627a6
Mårten Åsberg <marten.asberg@outlook.com>
2025-07-06 11:13:47 +0200

    Harden server project

--- RcCar.WebApi/CameraService.cs
+++ RcCar.WebApi/CameraService.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Threading;
 using System.Threading.Channels;
 using System.Threading.Tasks;
